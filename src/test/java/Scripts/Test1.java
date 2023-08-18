1e6f1074d36e979c0ba976494e310d314e5a511f
sasikalagithub <sasikalaManthena28@gmail.com>
2023-08-18 18:47:57 +0530

    changes

---
+++ src/test/java/Scripts/Test1.java
@@ -0,0 +1,11 @@
+package Scripts;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+	@Test
+	public void tc1()
+	{
+		System.out.println("Hi");
+	}
+}
---
+++ src/test/java/Scripts/Test2.java
@@ -0,0 +1,11 @@
+package Scripts;
+
+import org.testng.annotations.Test;
+
+public class Test2 {
+@Test
+public void tc2()
+{
+	System.out.println("Hello");
+	}
+}
